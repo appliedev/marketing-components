1eccf72b3907d19811d65f5ac9f130ca76c2ef27
Aleks <42706841+aleks-rdea@users.noreply.github.com>
2024-09-06 13:39:49 +1000

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,1 +1,25 @@
-const message = "hello world"
+class MarComNav extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: 'open' });
+
+    const wrapper = document.createElement('nav');
+    wrapper.setAttribute('class', 'marcom-nav-container');
+
+    const text = document.createElement('p');
+    text.textContent = '...';
+
+    wrapper.appendChild(text);
+    shadow.appendChild(wrapper);
+
+    const style = document.createElement('style');
+    style.textContent = `
+      .marcom-nav-container {
+        color: red;
+      }
+    `;
+    shadow.appendChild(style);
+  }
+}
+
+customElements.define('marcom-nav', MarComNav);
